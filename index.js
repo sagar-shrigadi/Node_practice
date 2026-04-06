ebcda6eedac80d80d9b86c8564e60fb39f13ddfa
sagar-shrigadi <181492974+sagar-shrigadi@users.noreply.github.com>
2026-04-06 20:16:59 +0530

    feat: create express app and serve html using express at requested route

--- index.js
+++ index.js
@@ -1,57 +1,54 @@
-import http from "node:http";
+import express from "express";
 import fs from "node:fs";
 
-const hostname = "127.0.0.1";
-const port = 3000;
+const app = express();
+const PORT = 3000;
 
-const server = http.createServer((req, res) => {
-  if (req.url === "/") {
-    fs.readFile("./index.html", (err, data) => {
-      if (err) {
-        throw err;
-      } else {
-        console.log("home page successfully finished reading!");
-        res.statusCode = 200;
-        res.setHeader("content-type", "text/html");
-        res.end(data);
-      }
-    });
-  } else if (req.url === "/about") {
-    fs.readFile("./Pages/about.html", (err, data) => {
-      if (err) {
-        throw err;
-      } else {
-        console.log("about page successfully read!");
-        res.statusCode = 200;
-        res.setHeader("Content-type", "text/html");
-        res.end(data);
-      }
-    });
-  } else if (req.url === "/contact") {
-    fs.readFile("./Pages/contact.html", (err, data) => {
-      if (err) {
-        throw err;
-      } else {
-        console.log("contact page successfully read!");
-        res.statusCode = 200;
-        res.setHeader("Content-type", "text/html");
-        res.end(data);
-      }
-    });
-  } else {
-    fs.readFile("./Pages/404.html", (err, data) => {
-      if (err) {
-        throw err;
-      } else {
-        console.log("error page successfully read!");
-        res.statusCode = 200;
-        res.setHeader("Content-type", "text/html");
-        res.end(data);
-      }
-    });
-  }
+app.get("/", (req, res) => {
+  fs.readFile("./index.html", (err, data) => {
+    if (err) {
+      throw err;
+    } else {
+      res.statusCode = 200;
+      res.setHeader("Content-type", "text/html");
+      res.send(data);
+    }
+  });
+});
+app.get("/about", (req, res) => {
+  fs.readFile("./Pages/about.html", (err, data) => {
+    if (err) {
+      throw err;
+    } else {
+      res.setHeader("Content-type", "text/html");
+      res.send(data);
+    }
+  });
+});
+app.get("/contact", (req, res) => {
+  fs.readFile("./Pages/contact.html", (err, data) => {
+    if (err) {
+      throw err;
+    } else {
+      res.setHeader("Content-type", "text/html");
+      res.send(data);
+    }
+  });
+});
+app.get("/*path", (req, res) => {
+  fs.readFile("./Pages/404.html", (err, data) => {
+    if (err) {
+      throw err;
+    } else {
+      res.setHeader("Content-type", "text/html");
+      res.send(data);
+    }
+  });
 });
 
-server.listen(port, hostname, () => {
-  console.log(`server running at ${hostname}:${port}`);
+app.listen(PORT, (err) => {
+  if (err) {
+    throw err;
+  }
+  console.log(`Successfully running server on port: ${PORT}`);
 });
